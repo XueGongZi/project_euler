24d1bd10e5cf96d4cfb1535080728ba6f40bfa85
Qixiang Chen <c.qixiang@hotmail.com>
2020-03-18 14:50:44 +0800

    find answer

--- problem5.py
+++ problem5.py
@@ -1,6 +1,9 @@
 # Problem 5, Smallest multiple
 #
 # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder. What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+import math_definition as math
+import numpy as np
+limit = 20
 
 def number_for_n_with_highest_power_within_m(n, m):
     p = 0
@@ -10,4 +13,7 @@ def number_for_n_with_highest_power_within_m(n, m):
         else:
             p += 1
 
-print(number_for_n_with_highest_power_within_m(2, 20))
+def ans():
+    list_of_primes = list(filter(math.is_prime, range(2, limit + 1)))
+    list_of_multiplies = list(map(lambda x: number_for_n_with_highest_power_within_m(x, limit), list_of_primes))
+    return np.prod(list_of_multiplies)
