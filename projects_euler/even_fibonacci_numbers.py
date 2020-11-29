8074049985c0fdabfb577405bd4b41bf3df740ca
XueGongZi <c.qixiang@hotmail.com>
2020-11-29 13:18:08 +0800

    even fibonacci numbers correct

--- projects_euler/even_fibonacci_numbers.py
+++ projects_euler/even_fibonacci_numbers.py
@@ -1,4 +1,13 @@
 class Solution:
-    def 
-    def sum_even_fibonacci(self):
+    def fibonacci_numbers(self, upper):
+        fib = [1, 1]
+        while fib[-1] <= upper:
+            new_element = fib[-1] + fib[-2]
+            fib.append(new_element)
+        return fib[:-1]
 
+    def sum_even_fibonacci(self, upper):
+        return sum(self.fibonacci_numbers(upper)) // 2
+
+upper = 4000000
+print(Solution().sum_even_fibonacci(upper))
