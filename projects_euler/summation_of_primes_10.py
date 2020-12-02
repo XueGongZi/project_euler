320bd31f44684176ca2f93b08a08f82bce97d9a6
XueGongZi <c.qixiang@hotmail.com>
2020-12-02 23:19:45 +0800

    change name

---
+++ projects_euler/summation_of_primes_10.py
@@ -0,0 +1,8 @@
+import 3_largest_prime_factor as prime
+
+class Solution:
+    def summation_of_primes(self, limit):
+        # limit is the upper limit
+        prime.is_prime(13)
+
+print(Solution().summation_of_primes())
